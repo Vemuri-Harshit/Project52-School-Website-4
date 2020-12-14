94e8ffde1b259b57ce910e64952917b980e9186e
Vemuri-Harshit <73873977+Vemuri-Harshit@users.noreply.github.com>
2020-12-14 11:12:56 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,38 @@
+/*.info,.extra{
+
+
+    display: flex;
+
+    flex-wrap:wrap;
+    
+    justify-content:space-evenly;
+
+
+}
+
+.extra1{
+
+ 
+
+background-color:orange;    
+
+border:solid;
+
+border-color:yellow;
+
+}
+
+.extra2{
+
+ 
+
+    background-color:orange;    
+    
+    border:solid;
+    
+    border-color:yellow;
+    
+    }
+
+
+*/ 
